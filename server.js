4eaf7f837641d479ca59f7513be56ea4affe7933
tharun8988 <149046475+tharun8988@users.noreply.github.com>
2026-09-03 15:06:24 +0530

    Set trust proxy for Express app

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+app.set('trust proxy', 1);
 
 const mongoose = require('mongoose');
 const cors = require('cors');
