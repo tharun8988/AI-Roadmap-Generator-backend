48333de86c497fcbd1ff1fbf42bc006828b9f36e
tharun8988 <149046475+tharun8988@users.noreply.github.com>
2026-09-03 10:52:05 +0530

    Add new allowed origin for Vercel frontend

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,8 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000',
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'https://ai-roadmap-generator-frontend.vercel.app'
 ]
 
 module.exports = allowedOrigins;
