7262ec44e76ff9e3916c9863106d19fc7818b804
tharun8988 <149046475+tharun8988@users.noreply.github.com>
2026-09-03 14:54:01 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -4,7 +4,8 @@ const allowedOrigins = [
     'http://localhost:3500',
     'http://localhost:3000',
     'http://localhost:5173',
-    'https://ai-roadmap-generator-frontend.vercel.app'
+    'https://ai-roadmap-generator-frontend.vercel.app',
+    'https://ai-roadmap-generator-backend.onrender.com'
 ]
 
 module.exports = allowedOrigins;
