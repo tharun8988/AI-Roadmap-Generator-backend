88e723161b82e2dd880f656a6ad82f516552650a
tharun8988 <149046475+tharun8988@users.noreply.github.com>
2026-09-03 15:30:05 +0530

    Update verifyJwt.js

--- middleware/verifyJwt.js
+++ middleware/verifyJwt.js
@@ -1,32 +1,27 @@
 const jwt = require("jsonwebtoken");
 
 const verifyJwt = (req, res, next) => {
+    const authHeader = req.headers.authorization || req.headers.Authorization;
+    let token = null;
 
-    const token = req.cookies.accessToken;
+    if (authHeader?.startsWith("Bearer ")) {
+        token = authHeader.split(" ")[1];
+    } else if (req.cookies?.accessToken) {
+        token = req.cookies.accessToken;
+    }
 
     if (!token) {
         return res.sendStatus(401);
     }
 
-    console.log("Access token from cookie:", token);
-
     jwt.verify(
         token,
         process.env.ACCESS_TOKEN,
         (err, decoded) => {
-
-            if (err) {
-                return res.sendStatus(403);
-            }
-
-            console.log(
-                "decoded user info:",
-                decoded.UserInfo
-            );
+            if (err) return res.sendStatus(403);
 
             req.mail = decoded.UserInfo.mail;
             req.UserInfo = decoded.UserInfo;
-
             next();
         }
     );
